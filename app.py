4cec660edb9d14e3cf3ee17d9467b415d57802f0
AIFattahMusic <oksip1520@gmail.com>
2026-01-31 13:51:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -135,7 +135,7 @@ def generate_status(task_id: str):
     state = item.get("state") or item.get("status")
     audio_url = item.get("audio_url") or item.get("audioUrl") or item.get("audio")
 
-     if state == "succeeded" and audio_url:
+    if state == "succeeded" and audio_url:
     audio_bytes = requests.get(audio_url).content
 
     with open("/media/song_1.mp3", "wb") as f:
@@ -171,6 +171,7 @@ app.mount("/media", StaticFiles(directory="media"), name="media")
 @app.get("/")
 def root():
     return {"ok": True}
+
 
 
 
